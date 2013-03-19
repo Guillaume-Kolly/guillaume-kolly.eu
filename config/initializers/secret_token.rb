988b6c68076c912917ebca08ff61dbb1ce00a8f3
Guillaume-Kolly <guillaume.kolly@gmail.com>
2013-03-19 01:00:56 +0100

    Base + mongoid + bootstrap

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GuillaumeKollyEu::Application.config.secret_token = 'e2271faf2eb137f7fdee4cde499f33fe38f9e19482ab23dbdc28650d75239dac8f6778be80dd9bd0f860b7a3ef1d30be72927fdfb577474041cbef252527fdf3'
